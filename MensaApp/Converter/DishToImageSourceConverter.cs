3497c3db02f0a6006c7c250229be960f6aafd632
auxua <arno.schmetz@rwth-aachen.de>
2026-04-04 21:58:47 +0200

    Changing Icons to consistent resources (#7)
    Updating Fonts, due to incorrect Platform handling
    Removing UI-aspects from Dish Model

---
+++ MensaApp/Converter/DishToImageSourceConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+using MensaPortable;
+
+
+namespace MensaApp.Converter
+{
+    
+    public class DishToImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Dish dish)
+            {
+                if (dish.Name == "Info")
+                {
+                    Application.Current.Resources.TryGetValue("IconMore", out object icon);
+                    return icon;
+                }
+                else if (dish.IsSideDish)
+                {
+                    Application.Current.Resources.TryGetValue("IconAddition", out object icon);
+                    return icon;
+                }
+                else
+                {
+                    Application.Current.Resources.TryGetValue("IconFood", out object icon);
+                    return icon;
+                }
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotSupportedException();
+    }
+}
